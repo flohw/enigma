0c8d4100ba8363777567feb59ef07eb59afe4773
Flohw <floparreno+github@gmail.com>
2015-06-25 21:30:50 +0200

    Check wiring when building the reflector

--- src/Enigma/Reflector.php
+++ src/Enigma/Reflector.php
@@ -26,5 +26,17 @@ class Reflector extends AbstractIndexedAlphabet
     public function __construct($wires)
     {
         $this->wires = $wires;
+
+        $this->checkWiring();
+    }
+
+    private function checkWiring()
+    {
+        foreach (range('A', 'Z') as $char) {
+            if ($char !== $this->reflect($this->reflect($char))) {
+                throw new \Exception(sprintf('%s => %s is not a legal reflection',
+                  $char, $this->reflect($this->reflect($char))));
+            }
+        }
     }
 }
