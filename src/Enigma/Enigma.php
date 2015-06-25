75eca6032fdfea5e1795943a5555c5ea7bb4f0e3
Flohw <floparreno+github@gmail.com>
2015-06-25 21:37:33 +0200

    Initialize Enigma

--- src/Enigma/Enigma.php
+++ src/Enigma/Enigma.php
@@ -9,5 +9,29 @@ namespace Enigma;
  */
 class Enigma
 {
+    /**
+     * @var Plugboard
+     */
+    private $pugboard;
 
+    /**
+     * @var Rotor[]
+     */
+    private $rotors;
+
+    /**
+     * @var Reflector
+     */
+    private $reflector;
+
+    /**
+     * Constructor
+     *
+     * @param Rotor[]        $rotors
+     * @param null|Reflector $reflector
+     * @param null|Plugboard $plugboard
+     */
+    public function __construct(array $rotors = array(), $reflector = null, $plugboard = null)
+    {
+    }
 }
