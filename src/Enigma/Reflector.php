3cec827f85fddd9e3cd0852ad32cc397238997b4
Flohw <floparreno+github@gmail.com>
2015-06-25 21:29:40 +0200

    Create Reflector class

---
+++ src/Enigma/Reflector.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Enigma;
+
+use Enigma\Component\AbstractIndexedAlphabet;
+
+/**
+ * Class Reflector
+ *
+ * @package Enigma
+ */
+class Reflector extends AbstractIndexedAlphabet
+{
+}
