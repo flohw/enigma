27797038dbfa577efecbbe560f40b344dd1e2fe7
Flohw <floparreno+github@gmail.com>
2015-06-25 21:41:50 +0200

    Implement Enigma constructor

--- src/Enigma/Enigma.php
+++ src/Enigma/Enigma.php
@@ -12,7 +12,7 @@ class Enigma
     /**
      * @var Plugboard
      */
-    private $pugboard;
+    private $plugboard;
 
     /**
      * @var Rotor[]
@@ -33,5 +33,60 @@ class Enigma
      */
     public function __construct(array $rotors = array(), $reflector = null, $plugboard = null)
     {
+        foreach ($rotors as $rotor) {
+            $this->addRotor($rotor);
+        }
+        $this->setReflector($reflector);
+        $this->setPlugboard($plugboard);
+    }
+
+    /**
+     * Add a rotor to the machine
+     *
+     * @param Rotor $rotor
+     *
+     * @return $this
+     */
+    public function addRotor(Rotor $rotor)
+    {
+        $this->rotors[] = $rotor;
+
+        return $this;
+    }
+
+    /**
+     * Set the machine's reflector
+     *
+     * @param Reflector $reflector
+     *
+     * @return $this
+     */
+    public function setReflector(Reflector $reflector = null)
+    {
+        if (null === $reflector) {
+            $reflector = new Reflector('EJMZALYXVBWFCRQUONTSPIKHGD');
+        }
+
+        $this->reflector = $reflector;
+
+        return $this;
+    }
+
+    /**
+     * Set the machine's plugboard
+     *
+     * @param Plugboard $plugboard
+     *
+     * @return $this
+     */
+    public function setPlugboard(Plugboard $plugboard = null)
+    {
+        if (null === $plugboard) {
+            $plugboard = new Plugboard();
+        }
+
+        $this->plugboard = $plugboard;
+
+        return $this;
     }
 }
