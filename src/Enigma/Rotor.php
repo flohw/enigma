e90c793fc9c60d339a6970aea388a5035cbc2ad0
Flohw <floparreno+github@gmail.com>
2015-06-25 21:00:30 +0200

    Construct the rotor

--- src/Enigma/Rotor.php
+++ src/Enigma/Rotor.php
@@ -11,4 +11,58 @@ use Enigma\Component\AbstractIndexedAlphabet;
  */
 class Rotor extends AbstractIndexedAlphabet
 {
+    /**
+     * @var string[]
+     */
+    private $wiring;
+
+    /**
+     * @var string[]
+     */
+    private $notch;
+
+    /**
+     * @var int
+     */
+    private $position;
+
+    /**
+     * Constructor
+     *
+     * @param string $wiring
+     * @param string $notch
+     * @param string|int $position
+     */
+    public function __construct($wiring, $notch, $position)
+    {
+        $this->wiring = $wiring;
+        $this->notch = $notch;
+        $this->setPosition($position);
+    }
+
+    /**
+     * Set rotor position
+     *
+     * @param string|int $position
+     */
+    public function setPosition($position)
+    {
+        if (!is_int($position)) {
+            $position = $this->charToInt($position);
+        }
+
+        $position %= 26;
+
+        while ($this->position !== $position) {
+            $this->rotate();
+        }
+    }
+
+    /**
+     * Rotate the rotor
+     */
+    public function rotate()
+    {
+        $this->position = ($this->position + 1) % 26;
+    }
 }
