562b79df2ce891815063c08c892769a26b922b97
Flohw <floparreno+github@gmail.com>
2015-06-25 19:06:21 +0200

    Construct Plugboard

--- src/Enigma/Plugboard.php
+++ src/Enigma/Plugboard.php
@@ -9,4 +9,24 @@ namespace Enigma;
  */
 class Plugboard
 {
+    /**
+     * @var string[]
+     */
+    private $plugboard;
+
+    /**
+     * Constructor
+     *
+     * @param string[] $plugboard
+     */
+    public function __construct(array $plugboard = array())
+    {
+        foreach ($plugboard as $from => $to) {
+            $from = strtoupper($from);
+            $to = strtoupper($to);
+
+            $this->plugboard[$from] = $to;
+            $this->plugboard[$to] = $from;
+        }
+    }
 }
