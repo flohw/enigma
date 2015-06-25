b83264d0c7ac9a8c03a9a09fb8e5a6c3ea960d59
Flohw <floparreno+github@gmail.com>
2015-06-25 21:29:59 +0200

    Construct the reflector

--- src/Enigma/Reflector.php
+++ src/Enigma/Reflector.php
@@ -11,4 +11,20 @@ use Enigma\Component\AbstractIndexedAlphabet;
  */
 class Reflector extends AbstractIndexedAlphabet
 {
+    /**
+     * @var string
+     */
+    private $wires;
+
+    /**
+     * Constructor
+     *
+     * @param string $wires
+     *
+     * @throws \Exception
+     */
+    public function __construct($wires)
+    {
+        $this->wires = $wires;
+    }
 }
