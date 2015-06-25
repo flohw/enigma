a3b38d8ab977f411457dfd0b4d88df2835942d09
Flohw <floparreno+github@gmail.com>
2015-06-25 19:33:41 +0200

    Throw exception when too many plugs

--- src/Enigma/Plugboard.php
+++ src/Enigma/Plugboard.php
@@ -17,10 +17,16 @@ class Plugboard
     /**
      * Constructor
      *
+     * @throws \Exception
+     *
      * @param string[] $plugboard
      */
     public function __construct(array $plugboard = array())
     {
+        if (count($plugboard) > 20) {
+            throw new \Exception('Plugboard can not be longer than 20 permutation');
+        }
+
         foreach ($plugboard as $from => $to) {
             $from = strtoupper($from);
             $to = strtoupper($to);
