212441416f58278f0eb7a641c56d8c5f7bf8c9cb
Flohw <floparreno+github@gmail.com>
2015-07-11 16:00:06 +0200

    Initilaize application

--- index.php
+++ index.php
@@ -2,20 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
-$enigma1 = new \Enigma\Enigma();
-$enigma1
-    ->addRotor(new \Enigma\Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q', 'A'))
-    ->addRotor(new \Enigma\Rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'E', 'A'))
-    ->addRotor(new \Enigma\Rotor('BDFHJLCPRTXVZNYEIWGAKMUSQO', 'V', 'Z'))
-    ->setPlugboard(new \Enigma\Plugboard(array('A' => 'T', 'S' => 'J')))
-    ->setReflector(new \Enigma\Reflector('EJMZALYXVBWFCRQUONTSPIKHGD'));
+use App\ApplicationCommand;
 
-$enigma2 = new \Enigma\Enigma();
-$enigma2
-    ->addRotor(new \Enigma\Rotor('EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'Q', 'A'))
-    ->addRotor(new \Enigma\Rotor('AJDKSIRUXBLHWTMCQGZNPYFVOE', 'E', 'A'))
-    ->addRotor(new \Enigma\Rotor('BDFHJLCPRTXVZNYEIWGAKMUSQO', 'V', 'Z'))
-    ->setPlugboard(new \Enigma\Plugboard(array('A' => 'T', 'S' => 'J')))
-    ->setReflector(new \Enigma\Reflector('EJMZALYXVBWFCRQUONTSPIKHGD'));
+use Symfony\Component\Console\Application;
 
-echo $enigma2->input($enigma1->input('MYXSECRETXMESSAGE')) . "\n";
+$application = new Application();
+$application->add(new ApplicationCommand());
+$application->run();
